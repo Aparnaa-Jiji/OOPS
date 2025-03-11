846e8f40a7d27de84bdac5449a5b5fd3c6643d7a
Aparnaa-Jiji <aparnajiji003@gmail.com>
2025-03-11 15:42:47 +0530

    Add files via upload

---
+++ Startstring.java
@@ -0,0 +1,8 @@
+class Startstring{
+    public static void main(String[] args) {
+        String s1= "Good Morning";
+        System.out.println(s1);
+        System.out.println("To uppercase:"+s1.toLowerCase());
+        System.out.println("To lowercase:"+s1.toUpperCase());
+    }
+}
