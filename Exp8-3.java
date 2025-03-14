6544dcde3cda978df4c2ad20c9ec2e1bedccb89d
Aparnaa-Jiji <aparnajiji003@gmail.com>
2025-03-15 00:28:12 +0530

    Update and rename Startstring.java to Exp8-3.java

--- Exp8-3.java
+++ Exp8-3.java
@@ -2,7 +2,7 @@ class Startstring{
     public static void main(String[] args) {
         String s1= "Good Morning";
         System.out.println(s1);
-        System.out.println("To uppercase:"+s1.toLowerCase());
-        System.out.println("To lowercase:"+s1.toUpperCase());
+        System.out.println("To uppercase:"+s1.toUpperCase());
+        System.out.println("To lowercase:"+s1.toLowerCase());
     }
 }
